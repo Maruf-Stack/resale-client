a4685f145a2bb367db00e40f5013200142652964
Maruf <01780527702maruf@gmail.com>
2022-11-25 13:50:20 +0600

    extra section added

--- src/pages/home/uique/Unique.js
+++ src/pages/home/uique/Unique.js
@@ -5,9 +5,9 @@ import discount from '../../../assets/discount.jpg'
 const Unique = () => {
     return (
         <div className='mt-16'>
-            <div className='text-center'>
-                <h3 className='text-4xl font-bold'>Buy car and get prize</h3>
-                <p className=''>Buy one car from us and you can get any one price of your choice</p>
+            <div className='mb-8 text-center'>
+                <h3 className='mb-2 text-4xl font-bold text-sky-400'>Buy one and get one free</h3>
+                <p className='font-semibold text-slate-500'>Buy one car from us and you can get any one price of your choice</p>
             </div>
             <div className='grid grid-cols-1 gap-2 lg:grid-cols-3'>
                 <img src={phone} alt="phone" className='mx-auto shadow-lg w-96 h-80' />
